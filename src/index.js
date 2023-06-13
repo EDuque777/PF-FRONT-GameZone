7484d189fe562ed627207be727fa97fabe4fc31a
fernandotejada1997 <fernandotejada1997@gmail.com>
2023-06-13 16:59:18 -0300

    prueba de commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom'
 import store from './redux/store'
 import { Provider } from 'react-redux'
 import axios from 'axios';
-
  //axios.defaults.baseURL = "http://localhost:3001/";
   axios.defaults.baseURL = "";
 
