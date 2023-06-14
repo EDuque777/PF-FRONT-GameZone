55142e4d67958b67d6780494bdca5abd9644f22f
Nicolas Campos <nikovic123@gmail.com>
2023-06-14 17:41:53 -0300

    cambio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+  content: ["./src/*/.{js,jsx,ts,tsx}",
   ],
+  theme: {
+    extend: {
+      colors:
+        {gray:{
+          med: "#313131",
+          light:"#141414",
+          dark:"#070707",
+          letters: "#A0A0A0"
+        }
+
+    }},
+  },
+  plugins: [],
 }
