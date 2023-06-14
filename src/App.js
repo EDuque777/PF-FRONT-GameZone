e63d8150e8fbe7f8268d1eeb6350dfdd1b7e5ffe
Nicolas Campos <nikovic123@gmail.com>
2023-06-14 11:29:49 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 import {useState, useEffect} from "react";
 import { useDispatch } from 'react-redux';
 import { Routes, Route, useLocation, useNavigate} from 'react-router-dom';
+import ShoppingCart from './views/shoppingCart/ShoppingCart';
 
 
 function App() {
 
   return (
     <div className="App">
+      <h1>AAAA</h1>
+      <ShoppingCart />
     </div>
   );
   
---
+++ src/views/shoppingCart/ShoppingCart.jsx
@@ -0,0 +1,26 @@
+//* Rellenar cuando se este realizando el redux e importar los componentes necesarios...
+import {} from "react";
+import {} from "react-redux";
+import * as act from "../../redux/actions";
+
+//! REVISAR COMO HACER QUE SE SUMEN LOS PRECIOS DE LOS PRODUCTOS
+//! REVISAR LA DIVISION Y EL RENDER
+//! RALIZAR LOS ACTIONS, REDUCER Y APLICAR EL PERSIST
+
+
+//* las cards que vengan del home...
+const ShoppingCart = () => {
+    return (
+        <div>
+            <h2>Carrito de Compras</h2>
+            <div>
+                <h3>Lista de productos:</h3>
+            </div>
+            <div>
+                //? componente card, videojuegos a guardar
+            </div>
+        </div>
+    )
+}
+
+export default ShoppingCart;
