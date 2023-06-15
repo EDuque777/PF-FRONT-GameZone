c281e1efc0767dd96ff338183b1ca7619fc1b9e5
JeroBl <jeroblanco44@gmail.com>
2023-06-15 12:09:06 -0300

    Detail, Redux, Card, CardsContainer

--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { clearDetail } from "../../redux/actions";
+import { clearDetail, getDetail } from "../../redux/actions";
 import style from "./Detail.module.css";
 
 const Detail = (props) => {
