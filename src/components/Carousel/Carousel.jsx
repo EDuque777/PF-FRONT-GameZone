5ca4178dd4a12342aa00da00ecc5e25d03f4eadd
fernandotejada1997 <fernandotejada1997@gmail.com>
2023-06-15 13:46:36 -0300

    cambios_home

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -11,7 +11,7 @@ console.log(offers)
 
 return(
     <div className={style.container}>
-        <h2 className={style.text}>carrousel</h2>
+        <h2 className={style.text}>   carrousel   </h2>
     </div>
 )
 }
