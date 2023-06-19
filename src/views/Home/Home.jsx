4cc4327e079ef1eb2e4d930aa6972d7624493e88
Nicolas Campos <nikovic123@gmail.com>
2023-06-19 16:04:23 -0300

    nada hecho casi

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -45,18 +45,18 @@ const [name, setName] = useState("")
                 <h1 className="text-gray-letters">HOME</h1>
                 <Carousel /> 
                 <div>
-                    {/* REVISAR BIEN HACER EL DISEÑO Y LA BUSQUEDA */}
-                    <input className={style.search} placeholder="   search..."type="text" onChange={e => handleChange(e)}  onKeyDown={handleKeyDown} onClick={(e) => {handleButton(e)}}/>
-                    <button > buscar</button>
+                    {/*//! REVISAR BIEN HACER EL DISEÑO Y LA BUSQUEDA */}
+                    <input className={style.search} placeholder="   search..." type="text" onChange={e => handleChange(e)} onKeyDown={handleKeyDown}/>
+                    <button type="submit" onClick={(e) => {handleButton(e)}} >buscar</button>
                 </div>
+                <h3 className={style.title}>All Games</h3>
+                <CardsContainer gameComingSoon={games} />
                 <h3 className={style.title}> Top sells </h3>
                 <CardsContainer gameComingSoon={gamesTopSellers} />
                 <h3 className={style.title}> Game offers </h3>
                 <CardsContainer gameComingSoon={gameOffer} />
                 <h3 className={style.title}> New releases </h3>
                 <CardsContainer gameComingSoon={gamesNewReleases} />
-                <h3>All Games</h3>
-                <CardsContainer gameComingSoon={games} />
         </div>
         
     );
