a6f72e94c7b11df28f8c5fd2d8f041c3d43f4137
fernandotejada1997 <fernandotejada1997@gmail.com>
2023-06-22 17:05:23 -0300

    flitrosysearch

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,97 +1,79 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as act from "../../redux/actions";
-import style from "./Filters.module.css"
-
+import style from "./Filters.module.css";
 
 const Filters = () => {
-
-    const dispatch = useDispatch();
-    
-    const gamesFilterPlatform = useSelector(state => state.gamesPlatforms);
-    const gamesFilterLanguages = useSelector(state => state.languagesGames);
-    const gamesFilterCategories = useSelector(state => state.categoriesGames);
-    const gamesFilterDevelopers = useSelector(state => state.developersGames);
-    const gamesFilterPublishers = useSelector(state => state.publishersGames);
-    const gamesFilterGenres = useSelector(state => state.genresGames);
-
-    useEffect(() => {
-      dispatch(act.platformsAll());
-      dispatch(act.languagesGames());
-      dispatch(act.categoriesGames());
-      dispatch(act.developersGames());
-      dispatch(act.publishersGames());
-      dispatch(act.genresGames());
-    }, [dispatch]);
+  const dispatch = useDispatch();
   
-    const platformFilter = gamesFilterPlatform.filter(platform => platform)
-    console.log(platformFilter)
+  const gamesFilterPlatform = useSelector(state => state.gamesPlatforms);
+  const gamesFilterLanguages = useSelector(state => state.languagesGames);
+  const gamesFilterCategories = useSelector(state => state.categoriesGames);
+  const gamesFilterDevelopers = useSelector(state => state.developersGames);
+  const gamesFilterPublishers = useSelector(state => state.publishersGames);
+  const gamesFilterGenres = useSelector(state => state.genresGames);
 
-    const languagesFilter = gamesFilterLanguages.filter(languages => languages)
-    console.log(languagesFilter)
+  useEffect(() => {
+    dispatch(act.platformsAll());
+    dispatch(act.languagesGames());
+    dispatch(act.categoriesGames());
+    dispatch(act.developersGames());
+    dispatch(act.publishersGames());
+    dispatch(act.genresGames());
+  }, [dispatch]);
 
-    const categoriesFilter = gamesFilterCategories.filter(categories => categories)
-    console.log(categoriesFilter)
+  const platformFilter = gamesFilterPlatform.filter(platform => platform);
+  const languagesFilter = gamesFilterLanguages.filter(languages => languages);
+  const categoriesFilter = gamesFilterCategories.filter(categories => categories);
+  const developersFilter = gamesFilterDevelopers.filter(developers => developers);
+  const publishersFilter = gamesFilterPublishers.filter(publishers => publishers);
+  const genresFilter = gamesFilterGenres.filter(genres => genres);
 
-    const developersFilter = gamesFilterDevelopers.filter(developers => developers)
-    console.log(developersFilter)
+  return (
+    <>
+      <select className={style.select}>
+        <option hidden>Platforms</option>
+        {platformFilter.map(({ platform, id }) => (
+          <option key={id} value={platform} className={style.option}>{platform}</option>
+        ))}
+      </select>
 
-    const publishersFilter = gamesFilterPublishers.filter(publishers => publishers)
-    console.log(publishersFilter)
+      <select className={style.select}>
+        <option hidden>Languages</option>
+        {languagesFilter.map(({ language, id }) => (
+          <option key={id} value={language} className={style.option}>{language}</option>
+        ))}
+      </select>
 
-    const genresFilter = gamesFilterGenres.filter(genres => genres)
-    console.log(genresFilter)
+      <select className={style.select}>
+        <option hidden>Categories</option>
+        {categoriesFilter.map(({ category, id }) => (
+          <option key={id} value={category} className={style.option}>{category}</option>
+        ))}
+      </select>
 
-    return (
-      <>
+      <select className={style.select}>
+        <option hidden>Developers</option>
+        {developersFilter.map(({ developer, id }) => (
+          <option key={id} value={developer} className={style.option}>{developer}</option>
+        ))}
+      </select>
 
-            <select>
-            <option hidden>Platforms</option>
-             {platformFilter.map(({platform, id}) => (
-              <option key={id} value={platform}>{platform}</option>
-            ))}
-            </select>
+      <select className={style.select}>
+        <option hidden>Publishers</option>
+        {publishersFilter.map(({ publisher, id }) => (
+          <option key={id} value={publisher} className={style.option}>{publisher}</option>
+        ))}
+      </select>
 
-            <select>
-            <option hidden>Languages</option>
-             {languagesFilter.map(({language, id}) => (
-              <option key={id} value={language}>{language}</option>
-            ))}
-            </select>
+      <select className={style.select}>
+        <option hidden>Genres</option>
+        {genresFilter.map(({ genre, id }) => (
+          <option key={id} value={genre} className={style.option}>{genre}</option>
+        ))}
+      </select>
+    </>
+  );
+};
 
-            <select>
-            <option hidden>Categories</option>
-             {categoriesFilter.map(({category, id}) => (
-              <option key={id} value={category}>{category}</option>
-            ))}
-            </select>
-
-            <select>
-            <option hidden>Developers</option>
-             {developersFilter.map(({developer, id}) => (
-              <option key={id} value={developer}>{developer}</option>
-            ))}
-            </select>
-
-            <select>
-            <option hidden>Publishers</option>
-             {publishersFilter.map(({publisher, id}) => (
-              <option key={id} value={publisher}>{publisher}</option>
-            ))}
-            </select>
-
-            <select>
-            <option hidden>Genres</option>
-             {genresFilter.map(({genre, id}) => (
-              <option key={id} value={genre}>{genre}</option>
-            ))}
-            </select>
-       
-      </>
-    );
-  };
-  
-  export default Filters;
-
-
-
+export default Filters;
--- src/views/Search/Search.jsx
+++ src/views/Search/Search.jsx
@@ -4,14 +4,15 @@ import style from "./Search.module.css";
 import * as act from "../../redux/actions";
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
 import { faSearchengin } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Pagination from "../../components/Pagination/Pagination";
+import Filters from "../../components/Filters/Filters";
 
 const Search = (props) => {
   const [name, setName] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [resultsPerPage, setResultsPerPage] = useState(8);
-  const search = useSelector(state => state.search);
+  const search = useSelector((state) => state.search);
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
@@ -49,7 +50,7 @@ const Search = (props) => {
             className={style.search}
             placeholder="  search..."
             type="text"
-            onChange={e => handleChange(e)}
+            onChange={(e) => handleChange(e)}
             onKeyDown={handleKeyDown}
           />
           <FontAwesomeIcon
@@ -58,21 +59,25 @@ const Search = (props) => {
             icon={faSearchengin}
             size="xl"
           />
+          <Filters />
         </div>
       </div>
       <div className={style.searchyfilters}>
-      <div className={style.results}>
-        <CardsContainer className={style.CardsContainer} gameComingSoon={currentResults} />
-        <Pagination
-          resultsPerPage={resultsPerPage}
-          totalResults={search.length}
-          currentPage={currentPage}
-          paginate={paginate}
-        />
-      </div>
-      <div className={style.filters}>
-        <h1></h1>
-      </div>
+        <div className={style.results}>
+          <CardsContainer
+            className={style.CardsContainer}
+            gameComingSoon={currentResults}
+          />
+          <Pagination
+            resultsPerPage={resultsPerPage}
+            totalResults={search.length}
+            currentPage={currentPage}
+            paginate={paginate}
+          />
+        </div>
+        <div className={style.filters}>
+          <h1>INSERTEFILTROS</h1>
+        </div>
       </div>
     </div>
   );
