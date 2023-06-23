a3517f4b6a29bcd283458fecc5932d2948f32edc
Murdock021 <diogomachadocmb@gmail.com>
2023-06-23 14:28:54 -0300

    Atualização

--- src/views/Adm/adm.jsx
+++ src/views/Adm/adm.jsx
@@ -3,13 +3,31 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Dashboard() {
+  //Form Game
   const [showForm, setShowForm] = useState(false);
   const [showUserForm, setShowUserForm] = useState(false);
-  const [nameGame, setNameGame] = useState('');
+  const [name, setName] = useState('');
+  const [type, setType] = useState('');
+  const [requiredAge, setRequiredAge] = useState('');
+  const [isFree, setIsFree] = useState('');
+  const [detailedDescription, setDetailedDescription] = useState('');
+  const [aboutTheGame, setAboutTheGame] = useState('');
+  const [shortDescription, setShortDescription] = useState('');
+  const [releaseDate, setReleaseDate] = useState('');
+  const [comingSoon, setComingSoon] = useState('');
+  const [supportInfo, setSupportInfo] = useState('');
+  const [metacritic, setMetacritic] = useState('');
+  const [priceOverview, setPriceOverview] = useState('');
+  const [headerImage, setHeaderImage] = useState('');
+  const [capsuleImage, setCapsuleImage] = useState('');
+  const [developers, setDevelopers] = useState('');
+  const [genres, setGenres] = useState('');
+  const [publishers, setPublishers] = useState('');
+  const [platform, setPlatform] = useState('');
   const [languages, setLanguages] = useState('');
-  const [minimumAge, setMinimumAge] = useState('');
-  const [description, setDescription] = useState('');
-  const [price, setPrice] = useState('');
+  const [categories, setCategories] = useState('');
+
+  // Form User
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -20,18 +38,48 @@ function Dashboard() {
     event.preventDefault();
     // Lógica para enviar os dados do formulário de criação de jogo
     console.log('Dados do formulário de criação de jogo:', {
-      nameGame,
+      name,
+      type,
+      requiredAge,
+      isFree,
+      detailedDescription,
+      aboutTheGame,
+      shortDescription,
+      releaseDate,
+      comingSoon,
+      supportInfo,
+      metacritic,
+      priceOverview,
+      headerImage,
+      capsuleImage,
+      developers,
+      genres,
+      publishers,
+      platform,
       languages,
-      minimumAge,
-      description,
-      price
+      categories
     });
     // Limpar os campos do formulário após o envio
-    setNameGame('');
+    setName('');
+    setType('');
+    setRequiredAge('');
+    setIsFree('');
+    setDetailedDescription('');
+    setAboutTheGame('');
+    setShortDescription('');
+    setReleaseDate('');
+    setComingSoon('');
+    setSupportInfo('');
+    setMetacritic('');
+    setPriceOverview('');
+    setHeaderImage('');
+    setCapsuleImage('');
+    setDevelopers('');
+    setGenres('');
+    setPublishers('');
+    setPlatform('');
     setLanguages('');
-    setMinimumAge('');
-    setDescription('');
-    setPrice('');
+    setCategories('');
   };
 
   const handleUserSubmit = (event) => {
@@ -121,29 +169,104 @@ function Dashboard() {
               <form onSubmit={handleGameSubmit}>
                 <label>
                   Name Game:
-                  <input type="text" value={nameGame} onChange={(e) => setNameGame(e.target.value)} />
+                  <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
                 </label>
-                <br />
+
+                <label>
+                  Type:
+                  <input type="text" value={type} onChange={(e) => setType(e.target.value)} />
+                </label>
+                
+                <label>
+                  Required Age:
+                  <input type="number" value={requiredAge} onChange={(e) => setRequiredAge(e.target.value)} />
+                </label>
+                
+                <label>
+                  Is Free:
+                  <input type="text" value={isFree} onChange={(e) => setIsFree(e.target.value)} />
+                </label>
+                
+                <label>
+                  Detailed Description:
+                  <textarea value={detailedDescription} onChange={(e) => setDetailedDescription(e.target.value)} />
+                </label>
+                
+                <label>
+                  About the Game:
+                  <textarea value={aboutTheGame} onChange={(e) => setAboutTheGame(e.target.value)} />
+                </label>
+               
+                <label>
+                  Short Description:
+                  <textarea value={shortDescription} onChange={(e) => setShortDescription(e.target.value)} />
+                </label>
+                
+                <label>
+                  Release Date:
+                  <input type="text" value={releaseDate} onChange={(e) => setReleaseDate(e.target.value)} />
+                </label>
+                
+                <label>
+                  Coming Soon:
+                  <input type="text" value={comingSoon} onChange={(e) => setComingSoon(e.target.value)} />
+                </label>
+                
+                <label>
+                  Support Info:
+                  <textarea value={supportInfo} onChange={(e) => setSupportInfo(e.target.value)} />
+                </label>
+                
+                <label>
+                  Metacritic:
+                  <input type="text" value={metacritic} onChange={(e) => setMetacritic(e.target.value)} />
+                </label>
+                
+                <label>
+                  Price Overview:
+                  <input type="text" value={priceOverview} onChange={(e) => setPriceOverview(e.target.value)} />
+                </label>
+               
+                <label>
+                  Header Image:
+                  <input type="text" value={headerImage} onChange={(e) => setHeaderImage(e.target.value)} />
+                </label>
+                
+                <label>
+                  Capsule Image:
+                  <input type="text" value={capsuleImage} onChange={(e) => setCapsuleImage(e.target.value)} />
+                </label>
+                
+                <label>
+                  Developers:
+                  <input type="text" value={developers} onChange={(e) => setDevelopers(e.target.value)} />
+                </label>
+                
+                <label>
+                  Genres:
+                  <input type="text" value={genres} onChange={(e) => setGenres(e.target.value)} />
+                </label>
+                
+                <label>
+                  Publishers:
+                  <input type="text" value={publishers} onChange={(e) => setPublishers(e.target.value)} />
+                </label>
+                
+                <label>
+                  Platform:
+                  <input type="text" value={platform} onChange={(e) => setPlatform(e.target.value)} />
+                </label>
+                
                 <label>
                   Languages:
                   <input type="text" value={languages} onChange={(e) => setLanguages(e.target.value)} />
                 </label>
-                <br />
+                
                 <label>
-                  Minimum Age:
-                  <input type="number" value={minimumAge} onChange={(e) => setMinimumAge(e.target.value)} />
+                  Categories:
+                  <input type="text" value={categories} onChange={(e) => setCategories(e.target.value)} />
                 </label>
-                <br />
-                <label>
-                  Description:
-                  <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                  Price:
-                  <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
-                </label>
-                <br />
+                
                 <input className={styles.button2} type="submit" value="Create Game" />
               </form>
             </div>
