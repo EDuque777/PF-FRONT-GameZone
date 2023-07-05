33d5817202f73c56f6af839f11f0b9f6f3b4846a
Nicolas Campos <nikovic123@gmail.com>
2023-07-04 21:20:02 -0300

    parseado

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
-import './index.css';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-import store from './redux/store'
-import { Provider } from 'react-redux'
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import "./index.css";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 axios.defaults.baseURL = "http://localhost:3001/";
 //axios.defaults.baseURL = "https://pf-back-gamezone-production.up.railway.app/"
 //axios.defaults.baseURL = "https://servidor-gamezone.onrender.com/"
 
 ReactDOM.render(
-
-<React.StrictMode>
-  <PayPalScriptProvider options={{"clientId" : "id del cliente"}}>
-    <Provider store={store}>
+  <React.StrictMode>
+    <PayPalScriptProvider options={{ clientId: "id del cliente" }}>
+      <Provider store={store}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-    </Provider>
-  </PayPalScriptProvider>
-</React.StrictMode>,
-  
-  document.getElementById('root')
-  
+      </Provider>
+    </PayPalScriptProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
